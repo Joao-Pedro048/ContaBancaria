7ab052a1675ad6fffbcc670a0bd78a3a883e08de
Joao-Pedro048 <soncinjoao@gmail.com>
2023-02-28 14:58:00 +0000

    java

--- programa.java
+++ programa.java
@@ -2,5 +2,18 @@ class programa {
     public static void main(String[] args){
         Conta minhaConta;
         minhaConta = new Conta();
+        minhaConta.numero=1;
+        minhaConta.titular="João Pedro";
+        minhaConta.saldo=2000;
+
+        Conta minhaConta2;
+        minhaConta2 = new Conta();
+        minhaConta2.numero=2;
+        minhaConta2.titular="camila";
+        minhaConta2.saldo=3000;
+
+        System.out.println(minhaConta.numero);
+        System.out.println(minhaConta.titular);
+        System.out.println(minhaConta.saldo);
     }
 }
